7cc1b53f16889ae86b26a31c18f8bc0a6f011bad
Yoel31 <yoel301@gmail.com>
2020-02-20 22:11:43 +0200

    good try

--- src/screens/RootNavigation.js
+++ src/screens/RootNavigation.js
@@ -4,13 +4,13 @@ let navigator;
 
 // nav is coming from react navigation
 export const setNavigator = navRef => {
-  console.log('navigation ref', navRef);
+  // console.log('navigation ref', navRef);
   // set navigator
   navigator = navRef;
 };
 
 export const navigate = (routeName, params) => {
-  console.log('[navigate dispatch] navigator', navigator);
+  // console.log('[navigate dispatch] navigator', navigator);
   navigator.dispatch(
     NavigationActions.navigate({
       routeName,
