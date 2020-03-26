db2bfae9b1cd9ebba33d2978a804691d9c5de621
Yoel31 <yoel301@gmail.com>
2020-03-26 23:20:17 +0200

    add travis file

--- file.bat
+++
@@ -1,6 +0,0 @@
-@ECHO ON
-ECHO שלום עולם!
-CD (.\android) && ^
-gradlew clean && ^
-CD (D:\projeckts\dell\deel) && ^
-react-native run-android
--- file2.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-ECHO Congratulations! Your first batch file executed successfully.
-CD (./android/) && ^
-./gradlew clean 
