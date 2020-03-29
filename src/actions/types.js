ddd95ce34d3d280d930f90711f41dda90f903497
Yoel31 <yoel301@gmail.com>
2020-03-29 12:58:03 +0300

    check travis

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,6 +1,6 @@
-export const LOGIN_USER_SUCCESS = "login_user_success";
-export const LOGOUT_USER_SUCCESS = "logout_user_success";
-export const LOGIN_USER_FAIL = "login_user_fail";
-export const LOGOUT_USER_FAIL = "logout_user_fail";
-export const LOGIN_USER = "login_user";
-export const LOGOUT_USER = "logout_user";
+export const LOGIN_USER_SUCCESS = 'login_user_success';
+export const LOGOUT_USER_SUCCESS = 'logout_user_success';
+export const LOGIN_USER_FAIL = 'login_user_fail';
+export const LOGOUT_USER_FAIL = 'logout_user_fail';
+export const LOGIN_USER = 'login_user';
+export const LOGOUT_USER = 'logout_user';
